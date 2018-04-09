81256777be77bd5b24e371df75274c974279a7fc
chenwenshu <wenshu_chen@mymail.sutd.edu.sg>
2018-04-09 18:22:11 +0800

    Update Image Labeling Software.py

--- Application/Image Labeling Software.py
+++ Application/Image Labeling Software.py
@@ -9,18 +9,23 @@ import sqlite3  # Import the SQLite3 module
 
 class MainApplication(Tk.Tk):
 	image = photo = []
-
+	
 	def __init__(self):
 		Tk.Tk.__init__(self)
 		self.path = Tk.StringVar()
-		self.db_link = "D:\GitHub\Labeling-Backend\Application\lkydata.db"
-
+		self.db_link = "lkydata.db"
+		
 		self.cb_ls = []  # checkbox list: [(<Tkinter.Checkbutton instance>,<Tkinter.IntVar instance>),......]
 		self.cb_data_ls = []  # checkbox data list: [(<Tkinter.Checkbutton instance>,1),......]
 		self.file_ls = []  # image list: [(<Tkinter.Checkbutton instance>,<Tkinter.IntVar instance>),......]
 		self.file_chosen_ls = []  # image data list: [<Tkinter.Checkbutton instance>,......]
 		self.datetime_ls = []
-		self.lbl_ls = ['person','home','playground','void_deck','park','public_space','supermarket','market','food_court','shop','mall','hospital','clinic','community_center','senior','religious','transaction_ser','fitness','bus_stop','mrt','walkway','pedestrian_crossing','cycling_path','street_lights','traffic_lights','street_signs','trees','furniture','stairs','ramps','walk','cycle','bus','train','car','drive','sit','chat','eat','shopping','run','exercise','not_useful']
+		self.lbl_ls = ['person', 'home', 'playground', 'void_deck', 'park', 'public_space', 'supermarket', 'market',
+		               'food_court', 'shop', 'mall', 'hospital', 'clinic', 'community_center', 'senior', 'religious',
+		               'transaction_ser', 'fitness', 'bus_stop', 'mrt', 'walkway', 'pedestrian_crossing',
+		               'cycling_path', 'street_lights', 'traffic_lights', 'street_signs', 'trees', 'furniture',
+		               'stairs', 'ramps', 'walk', 'cycle', 'bus', 'train', 'car', 'drive', 'sit', 'chat', 'eat',
+		               'shopping', 'run', 'exercise', 'not_useful']
 		self.lbl_dict = {"Person": "person",
 		                 "Home": "home",
 		                 "Playground/Fitness corner (outdoor)": "playground",
@@ -64,69 +69,75 @@ class MainApplication(Tk.Tk):
 		                 "Running": "run",
 		                 "Other exercising activity": "exercise",
 		                 "Not Useful": "not_useful"}
-
+		
 		self.title("Photo Labelling Software")
 		self.attributes('-fullscreen', True)
 		self.grid_rowconfigure(1, weight = 1)
 		self.grid_columnconfigure(0, weight = 1)
-
+		
 		# image_frame holds the images and is within left_frame
 		self.left_frame = Tk.Frame(self.master)
 		self.image_frame = Tk.Frame(self.master)
 		self.left_frame.grid(row = 0, sticky = "nsew")
 		self.image_frame.grid(row = 1, sticky = "nsew")
-
+		
 		self.canvas = Tk.Canvas(self.image_frame)
-		self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
 		self.frame_in2 = Tk.Frame(self.canvas)
 		self.my_scrollbar = Tk.Scrollbar(self.image_frame, orient = "vertical", command = self.canvas.yview)
 		self.canvas.configure(yscrollcommand = self.my_scrollbar.set)
 		self.my_scrollbar.pack(side = "right", fill = "y")
-
-		self.scroll_horizontal = Tk.Scrollbar(self.image_frame, orient="horizontal", command = self.canvas.xview)
+		self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+		
+		self.scroll_horizontal = Tk.Scrollbar(self.image_frame, orient = "horizontal", command = self.canvas.xview)
 		self.canvas.configure(xscrollcommand = self.scroll_horizontal.set)
-		self.scroll_horizontal.pack(side = "bottom", fill ="x")
+		self.scroll_horizontal.pack(side = "bottom", fill = "x")
 		self.canvas.pack(side = "left")
-
+		
 		self.canvas.create_window((0, 0), window = self.frame_in2, anchor = 'nw')
-
+		
 		self.frame_in2.bind("<Configure>", self.my_canvas)
-
+		
 		self.path_label = Tk.Label(self.left_frame, text = "Path  :   ").grid(row = 0, column = 0)
 		self.entry = Tk.Entry(self.left_frame, textvariable = self.path, width = 30).grid(row = 0, column = 1)
-
-		self.b1 = Tk.Button(self.left_frame, text = "Select", bg = "SkyBlue1", command = self.open_photo).grid(row = 0,column = 2, padx=2)
-		self.b2 = Tk.Button(self.left_frame, text = "Update", bg = "Olivedrab1", command = self.update).grid(row = 0,column = 3, padx=2)
-		self.b3 = Tk.Button(self.left_frame, text = "Show labels", command = self.show_lbl).grid(row = 0, column = 4, padx=2)
-		self.b4 = Tk.Button(self.left_frame, text = "Clear checks", command = self.clear_check).grid(row = 0,column = 5, padx=2)
-		self.b5 = Tk.Button(self.left_frame, text = "Clear attribute", command = self.clear_atribute).grid(row = 0, column = 6, padx=2)
-
+		
+		self.b1 = Tk.Button(self.left_frame, text = "Select", bg = "SkyBlue1", command = self.open_photo)\
+			.grid(row = 0, column = 2, padx = 2)
+		self.b2 = Tk.Button(self.left_frame, text = "Update", bg = "Olivedrab1", command = self.update)\
+			.grid(row = 0, column = 3, padx = 2)
+		self.b3 = Tk.Button(self.left_frame, text = "Show labels", command = self.show_lbl)\
+			.grid(row = 0, column = 4, padx = 2)
+		self.b4 = Tk.Button(self.left_frame, text = "Clear checks", command = self.clear_check)\
+			.grid(row = 0, column = 5, padx = 2)
+		self.b5 = Tk.Button(self.left_frame, text = "Clear attribute", command = self.clear_attribute)\
+			.grid(row = 0, column = 6, padx = 2)
+		
 		self.var_msg = Tk.StringVar()
-		self.msg_lbl = Tk.Label(self.left_frame, textvariable=self.var_msg, font=("Helvetica", 12)).grid(row = 1, column = 0, columnspan = 10)
-
+		self.msg_lbl = Tk.Label(self.left_frame, textvariable = self.var_msg, font = ("Helvetica", 12))\
+			.grid(row = 1, column = 0, columnspan = 10)
+		
 		# right_frame is the frame that displays the tags
 		self.right_frame = Tk.Frame(self.master)
 		self.right_frame.grid(sticky = "ne", row = 1, column = 1)
 		self.tag_labels = ttk.Notebook(self.right_frame)
 		self.tag_labels.grid(sticky = "n")
 		self.gen_tag_labels()
-
+	
 	def gen_tag_labels(self):
 		general_tab = ttk.Frame(self.tag_labels)
 		location_tab = ttk.Frame(self.tag_labels)
 		activity_tab = ttk.Frame(self.tag_labels)
 		others_tab = ttk.Frame(self.tag_labels)
-
+		
 		self.tag_labels.add(general_tab, text = "General")
 		self.tag_labels.add(location_tab, text = "Location/Built Environment Features")
 		self.tag_labels.add(activity_tab, text = "Activity")
 		self.tag_labels.add(others_tab, text = "Others")
-
+		
 		# General Tab
 		var = Tk.IntVar()
 		person = Tk.Checkbutton(general_tab, text = "Person", variable = var)
 		self.cb_ls.append((person, var))
-
+		
 		# Location Tab
 		var = Tk.IntVar()
 		home = Tk.Checkbutton(location_tab, text = "Home", variable = var)
@@ -135,8 +146,8 @@ class MainApplication(Tk.Tk):
 		playground = Tk.Checkbutton(location_tab, text = "Playground/Fitness corner (outdoor)", variable = var)
 		self.cb_ls.append((playground, var))
 		var = Tk.IntVar()
-		voiddeck = Tk.Checkbutton(location_tab, text = "Void deck", variable = var)
-		self.cb_ls.append((voiddeck, var))
+		void_deck = Tk.Checkbutton(location_tab, text = "Void deck", variable = var)
+		self.cb_ls.append((void_deck, var))
 		var = Tk.IntVar()
 		park = Tk.Checkbutton(location_tab, text = "Park", variable = var)
 		self.cb_ls.append((park, var))
@@ -178,7 +189,8 @@ class MainApplication(Tk.Tk):
 		services = Tk.Checkbutton(location_tab, text = "Transaction services (e.g. bank, post office)", variable = var)
 		self.cb_ls.append((services, var))
 		var = Tk.IntVar()
-		indoors = Tk.Checkbutton(location_tab, text = "Indoor fitness facility (e.g. sports hall, gym, swimming pool)", variable = var)
+		indoors = Tk.Checkbutton(location_tab, text = "Indoor fitness facility (e.g. sports hall, gym, swimming pool)",
+		                         variable = var)
 		self.cb_ls.append((hospital, var))
 		var = Tk.IntVar()
 		bus_stop = Tk.Checkbutton(location_tab, text = "Bus Stop/Interchange", variable = var)
@@ -208,7 +220,10 @@ class MainApplication(Tk.Tk):
 		trees = Tk.Checkbutton(location_tab, text = "Trees/Grass", variable = var)
 		self.cb_ls.append((trees, var))
 		var = Tk.IntVar()
-		publicgoods = Tk.Checkbutton(location_tab, text = "Public furniture (e.g. benches, tables, places related to to sitting/resting)", variable = var)
+		publicgoods = Tk.Checkbutton(location_tab,
+		                             text = "Public furniture (e.g. benches, tables, places related to to "
+		                                    "sitting/resting)",
+		                             variable = var)
 		self.cb_ls.append((publicgoods, var))
 		var = Tk.IntVar()
 		stairs = Tk.Checkbutton(location_tab, text = "Stairs and level changes", variable = var)
@@ -216,7 +231,7 @@ class MainApplication(Tk.Tk):
 		var = Tk.IntVar()
 		ramps = Tk.Checkbutton(location_tab, text = "Ramps", variable = var)
 		self.cb_ls.append((ramps, var))
-
+		
 		# Activity Tab
 		var = Tk.IntVar()
 		walk = Tk.Checkbutton(activity_tab, text = "Walking", variable = var)
@@ -254,18 +269,18 @@ class MainApplication(Tk.Tk):
 		var = Tk.IntVar()
 		others = Tk.Checkbutton(activity_tab, text = "Other exercising activity", variable = var)
 		self.cb_ls.append((others, var))
-
+		
 		# Others_tab
 		var = Tk.IntVar()
 		useless = Tk.Checkbutton(others_tab, text = "Not Useful", variable = var)
 		self.cb_ls.append((useless, var))
-
+		
 		for cb in self.cb_ls:
 			cb[0].grid(sticky = "w")
-
+	
 	def my_canvas(self, event):
 		self.canvas.configure(scrollregion = self.canvas.bbox("all"), width = 1000, height = 700)
-
+	
 	def open_photo(self):
 		path_ = tkFileDialog.askdirectory()
 		self.var_msg.set("Importing images...")
@@ -273,26 +288,34 @@ class MainApplication(Tk.Tk):
 		col = 0
 		row = 0
 		no_row = 4
-		self.file_ls = []
+		
+		self.image = []
+		self.photo = []
 		self.image_frame.update()
+		
+		self.frame_in2.destroy()
+		self.frame_in2 = Tk.Frame(self.canvas)
+		self.canvas.create_window((0, 0), window = self.frame_in2, anchor = 'nw')
+		self.frame_in2.bind("<Configure>", self.my_canvas)
+		
 		width = self.image_frame.winfo_width()
 		for file_name in os.listdir(self.path.get()):
 			global image, photo
 			if file_name.endswith(".jpg"):
-				self.image.insert(0, Image.open(os.path.join(self.path.get(), file_name)).resize((int(width/no_row-60), 145)))
+				self.image.insert(0, Image.open(os.path.join(self.path.get(), file_name)).resize(
+					(int(width / no_row - 60), 145)))
 				self.photo.insert(0, ImageTk.PhotoImage(self.image[0]))
 				var = Tk.IntVar()
 				c1 = Tk.Checkbutton(self.frame_in2, text = file_name, image = self.photo[0], compound = 'top',
-					                    variable = var, onvalue = 1, offvalue = 0)
+				                    variable = var, onvalue = 1, offvalue = 0)
 				c1.grid(row = row, column = col, sticky = Tk.W)
-				# c1.pack()
 				col += 1
 				if col >= no_row:
 					row += 1
 					col = 0
 				self.file_ls.append((c1, var))
 		self.var_msg.set("Imported images successfully")
-
+		
 		with sqlite3.connect(self.db_link) as db:
 			cur = db.cursor()
 			for x in self.file_ls:
@@ -302,46 +325,58 @@ class MainApplication(Tk.Tk):
 				row = cur.fetchall()
 				if len(row) != 0:
 					f.configure(bg = "tomato")
-
+					
+			db.commit()
+	
 	def update(self):
 		self.update_file_chosen_ls()
 		self.update_cb_data_ls()
-
+		
 		if len(self.file_chosen_ls) < 1:
 			self.var_msg.set("Please choose at least one image")
-
+		
 		else:
 			# update data to database
 			try:  # if file not exist in database, add new row
 				self.run_query("INSERT INTO attributes(file,date) VALUES (?,?)",
-							(self.file_chosen_ls[0].cget("text"), self.datetime_ls[0]))
+				               (self.file_chosen_ls[0].cget("text"), self.datetime_ls[0]))
 			except:  # if file exist in database, update it to all 0 first
 				self.run_query(
-					"UPDATE attributes SET person=0,home=0,playground=0,void_deck=0,park=0,public_space=0,supermarket=0,market=0,food_court=0,shop=0,mall=0,hospital=0,clinic=0,community_center=0,senior=0,religious=0,transaction_ser=0,fitness=0,bus_stop=0,mrt=0,walkway=0,pedestrian_crossing=0,cycling_path=0,street_lights=0,traffic_lights=0,street_signs=0,trees=0,furniture=0,stairs=0,ramps=0,walk=0,cycle=0,bus=0,train=0,car=0,drive=0,sit=0,chat=0,eat=0,shop=0,run=0,exercise=0,not_useful=0 WHERE file = (?)",
+					"UPDATE attributes SET person=0,home=0,playground=0,void_deck=0,park=0,public_space=0,"
+					"supermarket=0,market=0,food_court=0,shop=0,mall=0,hospital=0,clinic=0,community_center=0,"
+					"senior=0,religious=0,transaction_ser=0,fitness=0,bus_stop=0,mrt=0,walkway=0,"
+					"pedestrian_crossing=0,cycling_path=0,street_lights=0,traffic_lights=0,street_signs=0,trees=0,"
+					"furniture=0,stairs=0,ramps=0,walk=0,cycle=0,bus=0,train=0,car=0,drive=0,sit=0,chat=0,eat=0,"
+					"shop=0,run=0,exercise=0,not_useful=0 WHERE file = (?)",
 					(self.file_chosen_ls[0].cget("text"),))
-
+			
 			self.insert_row_data(self.cb_data_ls, 0)
-
+			
 			# Run if more than 1 picture being chosen
 			if len(self.file_chosen_ls) >= 2:
 				for j in range(1, len(self.file_chosen_ls)):
 					try:  # if file not exist in database, add new row
 						self.run_query("INSERT INTO attributes(file,date) VALUES (?,?)",
-									(self.file_chosen_ls[j].cget("text"), self.datetime_ls[j]))
+						               (self.file_chosen_ls[j].cget("text"), self.datetime_ls[j]))
 					except:  # if file exist in database, update it to all 0 first
 						self.run_query(
-							"UPDATE attributes SET person=0,home=0,playground=0,void_deck=0,park=0,public_space=0,supermarket=0,market=0,food_court=0,shop=0,mall=0,hospital=0,clinic=0,community_center=0,senior=0,religious=0,transaction_ser=0,fitness=0,bus_stop=0,mrt=0,walkway=0,pedestrian_crossing=0,cycling_path=0,street_lights=0,traffic_lights=0,street_signs=0,trees=0,furniture=0,stairs=0,ramps=0,walk=0,cycle=0,bus=0,train=0,car=0,drive=0,sit=0,chat=0,eat=0,shop=0,run=0,exercise=0,not_useful=0 WHERE file = (?)",
+							"UPDATE attributes SET person=0,home=0,playground=0,void_deck=0,park=0,public_space=0,"
+							"supermarket=0,market=0,food_court=0,shop=0,mall=0,hospital=0,clinic=0,community_center=0,"
+							"senior=0,religious=0,transaction_ser=0,fitness=0,bus_stop=0,mrt=0,walkway=0,"
+							"pedestrian_crossing=0,cycling_path=0,street_lights=0,traffic_lights=0,street_signs=0,"
+							"trees=0,furniture=0,stairs=0,ramps=0,walk=0,cycle=0,bus=0,train=0,car=0,drive=0,sit=0,"
+							"chat=0,eat=0,shop=0,run=0,exercise=0,not_useful=0 WHERE file = (?)",
 							(self.file_chosen_ls[j].cget("text"),))
-
+					
 					self.insert_row_data(self.cb_data_ls, j)
-
+			
 			# show feedback message
 			msg = str(len(self.file_chosen_ls)) + " Images was labelled with "
-			checked = False # A boolean represents if any label is checked
+			checked = False  # A boolean represents if any label is checked
 			for data in self.cb_data_ls:
 				label = self.lbl_dict[data[0].cget("text")]
 				k = data[1]  # k is a boolean
-				if checked == False:
+				if not checked:
 					if k == 1:
 						msg = msg + label + ", "
 						checked = True
@@ -350,7 +385,7 @@ class MainApplication(Tk.Tk):
 			msg = msg[0:-2] + "."
 			if checked == False:
 				self.var_msg.set("Please choose at least one attribute")
-
+			
 			else:
 				self.var_msg.set(msg)
 				# add red color to image
@@ -359,14 +394,14 @@ class MainApplication(Tk.Tk):
 					var = x[1]
 					if var.get() == 1:
 						f.configure(bg = "tomato")
-
+				
 				# clear img checks
 				for x in self.file_ls:
 					f = x[0]
 					var = x[1]
 					if var.get() == 1:
 						var.set(0)
-
+	
 	def update_file_chosen_ls(self):
 		# Take file_ls input and insert into file_chosen_ls
 		self.file_chosen_ls = []
@@ -377,9 +412,7 @@ class MainApplication(Tk.Tk):
 			if var.get() == 1:
 				self.file_chosen_ls.append(f)
 				self.datetime_ls.append(self.get_datetime(f))
-		#print self.file_chosen_ls
-		# print self.datetime_ls
-
+	
 	def update_cb_data_ls(self):
 		# Take cb_data input and insert into cb_data_ls
 		self.cb_data_ls = []
@@ -390,8 +423,7 @@ class MainApplication(Tk.Tk):
 				self.cb_data_ls.append((cb, 1))
 			else:
 				self.cb_data_ls.append((cb, 0))
-		# print self.cb_data_ls
-
+		
 	def insert_row_data(self, ls, n):
 		# get data from data_ls and insert into SQL table
 		for data in ls:
@@ -403,14 +435,14 @@ class MainApplication(Tk.Tk):
 				query_result = cur.execute("UPDATE attributes SET " + label + " = (?) WHERE file = (?)",
 				                           (1, self.file_chosen_ls[n].cget("text"),))
 				db.commit()
-
+	
 	def run_query(self, query, parameters = ()):
 		with sqlite3.connect(self.db_link) as db:
 			cur = db.cursor()
 			query_result = cur.execute(query, parameters)
 			db.commit()
 		return query_result
-
+	
 	def show_lbl(self):
 		self.update_file_chosen_ls()
 		if len(self.file_chosen_ls) == 0:
@@ -420,13 +452,15 @@ class MainApplication(Tk.Tk):
 		else:
 			img = self.file_chosen_ls[0]
 			name = img.cget("text")
-
+			
 			with sqlite3.connect(self.db_link) as db:
 				cur = db.cursor()
 				cur.execute("SELECT * FROM attributes WHERE file = (?)", (name,))
+				db.commit()
+				
 			row = cur.fetchall()
 			if len(row) == 0:
-				self.var_msg.set("Can't find "+ str(name) + " in database.")
+				self.var_msg.set("Can't find " + str(name) + " in database.")
 			else:
 				row = row[0][3:]
 				msg = str(name) + ": "
@@ -435,27 +469,29 @@ class MainApplication(Tk.Tk):
 						msg = msg + str(self.lbl_ls[i]) + ', '
 				msg = msg[0:-2] + "."
 				self.var_msg.set(msg)
-
+	
 	def get_datetime(self, f):
 		name = f.cget("text")  # 20000101_030548_000.jpg  -->  YYYY-MM-DD HH:MI:SS
 		try:
-			datetime = name[0:4] + "-" + name[4:6] + "-" + name[6:8] + " " + name[9:11] + ":" + name[11:13] + ":" + name[13:15]
+			datetime = name[0:4] + "-" + name[4:6] + "-" + name[6:8] + " " + name[9:11] + ":" + name[11:13] + ":" \
+			           + name[13:15]
 		except:
 			datetime = None
 		return datetime
-
-	def clear_atribute(self):
+	
+	def clear_attribute(self):
 		for x in self.cb_ls:
 			var = x[1]
 			var.set(0)
-
+	
 	def clear_check(self):
 		for y in self.file_ls:
 			var = y[1]
 			var.set(0)
-
+	
 	def _on_mousewheel(self, event):
-		self.canvas.yview_scroll(-1*(event.delta/120), "units")
+		self.canvas.yview_scroll(-1 * (event.delta / 120), "units")
+
 
 # print('hello world')
 root = MainApplication()
